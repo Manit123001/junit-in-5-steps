97d594f6f2f9296c3703ab83e161abc699cd6867
Manit Cholpinyo <captain.newclear@gmail.com>
2018-11-13 11:27:01 +0700

    Unit test for the sum method

--- test/com.in28minutes.junit/MyMathTest.java
+++ test/com.in28minutes.junit/MyMathTest.java
@@ -2,9 +2,23 @@ package com.in28minutes.junit;
 
 import org.junit.Test;
 
-public class MyMathTest{
-    @Test
-    public void test(){
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
+public class MyMathTest {
+    MyMath myMath = new MyMath();
+    //MyMath.sum_witkh3number
+    //1,2,3,4 => 6
+    @Test
+    public void sum_with3number() {
+        int result = myMath.sum(new int[]{1, 2, 3});
+        assertEquals(6, result);
+    }
+
+    @Test
+    public void sum_with1number() {
+        MyMath myMath = new MyMath();
+        int result = myMath.sum(new int[]{3});
+        assertEquals(3, result);
     }
 }
